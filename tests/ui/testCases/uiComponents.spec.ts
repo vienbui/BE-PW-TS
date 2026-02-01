2a84c37399eaf65c9bc4590c7dc1134e489217cf
Vien Bui <buivien314@gmail.com>
2026-02-01 16:23:46 +0700

    add input field test

---
+++ tests/ui/testCases/uiComponents.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+import { TextBoxPage } from '../pages/TextBoxPage';
+import { userData } from '../testData/userData';
+
+test.describe('Text Box', () => {
+
+    let textBoxPage: TextBoxPage;
+
+    test.beforeEach(async ({ page }) => {
+        textBoxPage = new TextBoxPage(page);
+        await textBoxPage.goto();
+    })
+
+    test('Fill out the form', async () => {
+       const user = userData.validUser;
+       await textBoxPage.fillForm(
+        user.name,
+        user.email,
+        user.currentAddress,
+        user.permanentAddress
+    );
+    await textBoxPage.submit();
+
+    // Assertions
+    await expect(textBoxPage.outputBox).toBeVisible();
+    await expect(textBoxPage.outputName).toContainText(user.name);
+    await expect(textBoxPage.outputEmail).toContainText(user.email);
+    })
+})
